1399fb23f82d4ae1a7a6dca37a90e69296817ce1
phongsiri309 <pongsiri@storemesh.com>
2022-12-02 16:44:05 +0700

    Update | some style on components

--- web/frontend/src/components/SlideOverlay/dsm-slideOverlay.vue
+++ web/frontend/src/components/SlideOverlay/dsm-slideOverlay.vue
@@ -17,12 +17,13 @@ const toggleOverlay = () => {
 <template>
   <div :class="`shadow ${overlay ? ' w-2/3 xl:w-1/4' : 'w-0 '
   }  absolute ${right ? 'right-0' : ''} duration-500 `">
-    <div :class="`px-1 py-7 shadow absolute  cursor-pointer ${right ? ' -left-7' : '-right-7'}`" @click="toggleOverlay">
+    <div :class="` px-2 py-5 shadow absolute  top-6  cursor-pointer ${right ? ' -left-7' : '-right-7'}`"
+      @click="toggleOverlay">
       <div :class="`${overlay ? right ? 'rotate-180' : '' : right ? '' : 'rotate-180'}`">
         <slot name="expandIcon"> <triangle-icon /> </slot>
       </div>
     </div>
-    <div :class="`${overlay ? ' ' : ' overflow-hidden'}  max-h-full  overflow-auto relative`">
+    <div :class="`${overlay ? ' ' : ' overflow-hidden'} bg-white  max-h-full  overflow-auto relative`">
       <slot name="content"> content Here ! </slot>
     </div>
   </div>

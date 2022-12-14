348dfb39c8303154c12cbafcc5c8360989656a6e
SothanaV <nai6320@gmail.com>
2022-12-14 16:51:01 +0700

    [ADD] path relation api

--- web/backend/searchapp/api/viewsets.py
+++ web/backend/searchapp/api/viewsets.py
@@ -15,7 +15,7 @@ class SearchViewset(viewsets.GenericViewSet,
     
     def list(self, request, *args, **kwargs):
         objs = utils.pgsearch.search(word=request.GET.get('q', ""), model=models.Entity, fields=['name'])
-        return Response(serializers.EntitySerializer(objs, many=True).data, status=status.HTTP_200_OK)
+        return Response(serializers.EntitySearchSerializer(objs, many=True).data, status=status.HTTP_200_OK)
     
 
 class EntityViewsets(viewsets.GenericViewSet,
@@ -25,6 +25,8 @@ class EntityViewsets(viewsets.GenericViewSet,
         return models.Entity.objects.all()
     
     def get_serializer_class(self):
+        if self.action == "relation":
+            return serializers.RelationSerializer
         return serializers.EntitySerializer
     
     def list(self, request, *args, **kwargs):
@@ -34,4 +36,12 @@ class EntityViewsets(viewsets.GenericViewSet,
             page=int(request.GET.get('page', "0"))
         )
         return Response(data, status=status.HTTP_200_OK)
-       
+    
+    @action(methods=['POST'], detail=False)
+    def relation(self, request, *args, **kwargs):
+        serializer = serializers.RelationSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            response = utils.graph.get_relation(ids=request.data.get('ids', []), hop=data.get('hop'))
+            return Response(response, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

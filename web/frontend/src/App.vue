b0de9c4f24a93735976051d107b7d4b8d6856e06
Nattawut-M <t.nattawut2020@hotmail.com>
2023-03-21 12:17:55 +0000

    [UPDATE] tab title, icon

--- web/frontend/src/App.vue
+++ web/frontend/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import webIcon from "./assets/imgs/faviconIcon.png";
+import webIcon from "./assets/imgs/CA-AppLogo.png";
 
 const favicon = document.getElementById("favicon");
 favicon.href = webIcon;
-document.title = "SEC";
+document.title = "CA";
 </script>
 
 <template>

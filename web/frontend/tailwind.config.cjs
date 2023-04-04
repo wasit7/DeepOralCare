b39c3853330bd7bb99d068dd8035036fa6f49ddd
Nattawut-M <t.nattawut2020@hotmail.com>
2023-04-04 04:13:36 +0000

    [ADD] tailwind global colors config

--- web/frontend/tailwind.config.cjs
+++ web/frontend/tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': '#194456',
+      'primary-light': '#3b8f85',
+      'secondary': '#f28606',
+      'secondary-light': '#ffb11d',
+      'background': '#dde3e5',
+    },
     extend: {},
   },
   plugins: [

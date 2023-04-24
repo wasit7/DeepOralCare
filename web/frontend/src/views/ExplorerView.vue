5d2382693e9bb249104efb6af4d52dc91d5c9482
Nattawut-M <t.nattawut2020@hotmail.com>
2023-04-24 08:23:48 +0000

    [ADD] button loading

--- web/frontend/src/views/ExplorerView.vue
+++ web/frontend/src/views/ExplorerView.vue
@@ -42,6 +42,8 @@ const resultExposure = ref(null);
 const searchLoading = reactive({
   disease: false,
   exposure: false,
+  result: false,
+
 });
 const searchInputTimeout = reactive({
   disease: null,
@@ -146,11 +148,14 @@ const onrightClickNode = async (id) => {
 };
 
 const onExplore = async () => {
+  searchLoading.result = true;
   const { id: diseaseId } = searchDisease.value;
   const { id: exposureId } = searchExposure.value;
   const ids = [diseaseId, exposureId].filter( (elem) => !!elem);
   console.log(`searching relationship between ${diseaseId} and ${exposureId} (${ids.length})`);
   await storeMain.getRelation(ids);
+  searchLoading.result = false;
+
 };
 
 const onSelection = (entity, entitySelected) => {
@@ -174,7 +179,7 @@ watch(searchDisease.value, (newValue) => {
   }, 700);
 });
 
-watch(searchExposure.value, (newValue) => {
+watch(searchExposure.value, (newValue) => { 
   clearTimeout(searchInputTimeout.exposure);
   searchLoading.exposure = true;
   searchInputTimeout.exposure = setTimeout(async () => {
@@ -237,11 +242,39 @@ watch(searchExposure.value, (newValue) => {
           />
 
           <button
+            id="btn-explore"
             type="button"
             @click="onExplore"
             class="focus:outline-none text-white bg-primary-light hover:bg-primary font-medium rounded-md text-sm mt-1 px-5 py-1.5"
+            :class="{
+              'opacity-75 cursor-wait': searchLoading.result,
+            }"
           >
-            Explore
+            <span v-if="!searchLoading.result">Explore</span>
+            <span
+              v-else
+              id="span-explore"
+              class="flex flex-row justify-center align-baseline"
+            >
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="inline w-4 h-4 mr-1 text-white animate-spin self-end"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#f28606"
+                />
+              </svg>
+              Loading...
+            </span>
           </button>
         </div>
       </template>
